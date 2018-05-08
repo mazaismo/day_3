c9bbec33ac85e217262318c595d1c06ebddbbd4e
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-08 19:27:31 +0300

    day_3
    majas_darbs

--- day_3/day_3/Program.cs
+++ day_3/day_3/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace day_3
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int skaitlis = Funkcija1();
+            Funkcija2();
+            Funkcija2();
+            Funkcija2();
+            Console.WriteLine(skaitlis);
+            Console.ReadLine();
+        }
+
+        static int Funkcija1() //sakaitisim a un b //
+        {
+            int a = 5;
+            int b = 10;
+
+
+            int c = a + b;
+
+            return c;
+        }
+
+        static void Funkcija2()
+        {
+            Console.WriteLine("apaļš ābols bumbierim cep čību");
+            Console.WriteLine("divmetrīgā eglē ērglis filmē gliemežu ģimeni");
+
         }
     }
 }
